74936dc3be5e564cf6ca63819cf1c3f60feaa1ba
pedrorijo91 <pedrorijo91@gmail.com>
2021-07-26 18:20:13 +0100

    Add basic service loader

--- java-service-loader/src/main/java/com/pedrorijo91/App.java
+++ java-service-loader/src/main/java/com/pedrorijo91/App.java
@@ -1,7 +1,18 @@
 package com.pedrorijo91;
 
+import java.util.*;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        ServiceLoader<MyService> serviceLoader = ServiceLoader.load(MyService.class);
+
+        List<MyService> services = new ArrayList<>();
+        for (MyService service : serviceLoader) {
+            System.out.println("I've found a service called '" + service.getName() + "' !");
+            services.add(service);
+        }
+
+        System.out.print("Found " + services.size() + " services!");
+
     }
 }
